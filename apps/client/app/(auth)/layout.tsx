5cdf383397619e2b901048eb57b28ec0a0a3e2a2
ArjunDahal999 <dahalarjun404@gmail.com>
2026-02-25 22:13:49 +0545

    fix client build

--- apps/client/app/(auth)/layout.tsx
+++ apps/client/app/(auth)/layout.tsx
@@ -1,10 +1,14 @@
+import { Suspense } from "react";
+
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col min-h-dvh items-center gap-y-2 justify-center">
       <h1 className=" text-5xl font-bold text-primary text-center">
         Event Planner
       </h1>
+          <Suspense fallback={<div className="p-4">Loading...</div>}>
       {children}
+          </Suspense>
     </div>
   );
 };
